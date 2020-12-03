5d20f5827226b16210a79cf82677b4894bccc7c7
Drew Thompson <drewallynthompson@gmail.com>
2020-12-03 09:28:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,10 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  
+  accepts_
 
-   def comments_attributes=(comment_attributes)
+  def comments_attributes=(comment_attributes)
     comment_attributes.values.each do |comment_attribute|
       comment = Comment.find_or_create_by(comment_attribute)
       self.comment_attributes.build(comment: comment)
