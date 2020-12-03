ed1ad86f6d64fd24813e7b405fde0eee21d1b653
Drew Thompson <drewallynthompson@gmail.com>
2020-12-03 09:29:28 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   
-  accepts_nested_attributes_for :user, reject_if: :all_blank
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
   def comments_attributes=(comment_attributes)
     comment_attributes.values.each do |comment_attribute|
